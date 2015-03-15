26fa027447453d1dafa57d6cc68bce55f6769716
Jstrida98 <jstrida98@gmail.com>
2015-03-15 00:42:07 -1000

    crutch-google: First commit, working on google dev server. Currently has a timer and crutch logo.

---
+++ crutch/crutch-war/src/main/java/com/crutchclothing/comingsoon/ComingSoonController.java
@@ -0,0 +1,25 @@
+package com.crutchclothing.comingsoon;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ComingSoonController {
+	
+	@RequestMapping(value="/soon", method = RequestMethod.GET)
+	public String getMovie(ModelMap model) {
+		System.out.println("TEST");
+		model.addAttribute("message", "test");
+ 
+		return "comingsoon";
+ 
+	}
+
+}
